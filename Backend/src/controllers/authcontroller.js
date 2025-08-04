59471bae56a433a48192892b5891248f21c11050
Humza920 <humza.rehman.779@gmail.com>
2025-08-04 21:23:47 +0500

    LOGIN AND LOGOUT CREATED (PROJECT FOR PORTFOLIO)

--- Backend/src/controllers/authcontroller.js
+++ Backend/src/controllers/authcontroller.js
@@ -33,11 +33,47 @@ exports.registeruser = async (req, res) => {
             secure: true,
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
-        res.status(201).json(user)
+        res.status(200).json(user)
     } catch (error) {
         res.status(500).json({ message: error.message })
     }
 }
 
-exports.loginuser = async (req , res) => {
+exports.loginuser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        if (!email) {
+            res.status(400).json({ message: "Enter your Email" })
+        }
+        if (!password) {
+            res.status(400).json({ message: "Enter your Password" })
+        }
+        const userchecking = await User.findOne({ email }.select("+password"))
+        if (!userchecking) {
+            res.status(400).json({ message: "User with this email does not exist" })
+        }
+        const hashPass = bcrypt.compare(password, userchecking.password)
+        if (!hashPass) {
+            res.status(400).json({ message: "Invalid Credentials" })
+        } else {
+            const token = generateToken(userchecking._id)
+            res.cookie("token", token, {
+                httpOnly: true,
+                sameSite: "strict",
+                secure: true,
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+            })
+
+            const { password, ...userwithoutpass } = userchecking._doc
+            res.status(200).json({ message: "Login Sucessfull" })
+            res.send(userwithoutpass)
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+exports.logout = async (req, res) => {
+    res.clearCookie("token")
+    res.send("logout successful")
 }
